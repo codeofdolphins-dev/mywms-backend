259b6949515c26cd47f74ab14f097ee10b9a325a
codeofdolphins-dev <cod.devgroup@gmail.com>
2025-07-22 18:57:16 +0530

    Fix dotenv installation for Render

--- src/db/config.js
+++ src/db/config.js
@@ -3,6 +3,7 @@ import { Sequelize } from "sequelize";
 
 const db_obj = new Sequelize(process.env.DATABASE_NAME, process.env.USER, process.env.PASSWORD, {
   host: process.env.HOST,
+  port: process.env.MYSQL_DB_PORT,
   dialect: 'mysql'
 });
 
