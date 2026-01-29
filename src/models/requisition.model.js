86d85bfd809637e3e602cb8f64cbbfe0a1987ab2
Kalyan0101 <naskarkalyan2000@outlook.com>
2026-01-29 23:37:37 +0530

    style: format requisition model for improved readability

--- src/models/requisition.model.js
+++ src/models/requisition.model.js
@@ -1,49 +1,49 @@
 import { DataTypes } from "sequelize";
 
 function Requisition(sequelize) {
-  return sequelize.define("Requisition", {
-    requisition_no: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    buyer_business_node_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    supplier_business_node_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    notes: {
-      type: DataTypes.TEXT,
-      allowNull: true
-    },
-    total: {
-      type: DataTypes.DECIMAL(10,2),
-      allowNull: true,
-      defaultValue: 0.00
-    },
-    required_by_date: {
-      type: DataTypes.DATEONLY,
-      allowNull: true,
-      defaultValue: 0.00
-    },
-    created_by: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    status: {
-      type: DataTypes.ENUM("draft", "submitted", "pending", "approved", "rejected", "cancelled"),
-      defaultValue: "draft"
-    },
-    priority: {
-        type: DataTypes.ENUM("low", "normal", "high"),
-        defaultValue: "normal"
-    },
-  });
+    return sequelize.define("Requisition", {
+        requisition_no: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        buyer_business_node_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        supplier_business_node_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        notes: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        total: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: true,
+            defaultValue: 0.00
+        },
+        required_by_date: {
+            type: DataTypes.DATEONLY,
+            allowNull: true,
+            defaultValue: 0.00
+        },
+        created_by: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        status: {
+            type: DataTypes.ENUM("draft", "submitted", "pending", "approved", "rejected", "cancelled"),
+            defaultValue: "draft"
+        },
+        priority: {
+            type: DataTypes.ENUM("low", "normal", "high"),
+            defaultValue: "normal"
+        },
+    });
 }
 export default Requisition;
