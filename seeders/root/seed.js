c91fe8f12dcd4a45be75a2c514f7b475b67d7fb1
Kalyan0101 <naskarkalyan2000@outlook.com>
2026-02-01 22:27:36 +0530

    remove migration files from main branch

--- seeders/root/seed.js
+++
@@ -1,9 +0,0 @@
-import { dataSeederRoot } from "../../src/helper/seeder.js";
-
-export async function seedRoot(models) {
-  const count = await models.Role.count();
-  if (count === 0) {
-    console.log("👑 🌱 Seeding root DB...");
-    await dataSeederRoot(models);
-  }
-}
--- seeders/tenant/seed.js
+++
@@ -1,6 +0,0 @@
-import { dataSeederTenant } from "../../src/helper/seeder.js";
-
-export async function seedTenant(models) {
-  console.log("👷 🌱 Seeding tenant DB...");
-  await dataSeederTenant(models);
-}
